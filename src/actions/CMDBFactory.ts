15913d619c01776e74f73c025940b3e9112ebd40
Gustaf Ankarloo <gankarloo@gmail.com>
2021-05-06 16:58:50 +0200

    add JSDoc

--- src/actions/CMDBFactory.ts
+++ src/actions/CMDBFactory.ts
@@ -11,6 +11,13 @@ import { CmdbBase } from "./CmdbBase";
 import { CmdbKangaroo } from "./Kangaroo/CmdbKangaroo";
 
 export class CMDBFactory {
+    /**
+     * 
+     * @param name 
+     * @param recordName 
+     * @param recordSize 
+     * @returns 
+     */
     public getCMDB(name: string, recordName: string, recordSize: number): CmdbBase {
         if (name.toLowerCase().indexOf("kangaroo") >= 0) {
             return new CmdbKangaroo(recordName, recordSize);
