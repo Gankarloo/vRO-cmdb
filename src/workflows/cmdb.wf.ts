561f0abd35fa64a082f7372950861a5dde895b1a
Gustaf Ankarloo <gankarloo@gmail.com>
2021-05-07 21:23:14 +0200

    misc

--- src/workflows/cmdb.wf.ts
+++ src/workflows/cmdb.wf.ts
@@ -14,7 +14,14 @@ import { CMDBFactory } from "../actions/CMDBFactory";
     name: "CMDB-Add",
     path: "CGI/Handson",
 })
+
 export class CMDBAdd {
+    /**
+     * 
+     * @param cmdbName 
+     * @param assetName 
+     * @param assetSize 
+     */
     public AddCMDB(cmdbName: string, assetName: string, assetSize: number) {
         let factory = new CMDBFactory();
         let cmdb = factory.getCMDB(cmdbName, assetName, assetSize);
@@ -27,6 +34,13 @@ export class CMDBAdd {
     path: "CGI/Handson",
 })
 export class CMDBRemove {
+    /**
+     * 
+     * @param cmdbName 
+     * @param assetName 
+     * @param assetSize 
+     * @param assetId 
+     */
     public RemoveCMDB(cmdbName: string, assetName: string, assetSize: number, assetId: number) {
         let factory = new CMDBFactory();
         let cmdb = factory.getCMDB(cmdbName, assetName, assetSize);
