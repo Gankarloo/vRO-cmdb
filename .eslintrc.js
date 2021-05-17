3dd8270b8d852ffe3a763fa283d109d6fc02a1cb
Gustaf Ankarloo <gankarloo@gmail.com>
2021-05-17 14:31:48 +0200

    eslint-ts

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    ],
+    rules: {
+        "@typescript-eslint/no-inferrable-types": "error",
+        "@typescript-eslint/no-non-null-assertion": "error",
+    },
+    overrides: [
+        {
+            files: ["src/**/*.ts"],
+            excludedFiles: ["*.test.ts", "*.spec.ts"],
+        }
+    ]
+};
+// docs https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md
